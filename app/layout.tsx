0ec126650cfc3f998fb6de8bc2601c2957c8baef
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2025-01-17 14:54:28 +0800

    Add Chinese font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from './providers'
 import Header from './header'
-import { jost } from '@/utils/fonts'
+import { chinese, jost } from '@/utils/fonts'
 import getHandle, { checkIsEmbedding } from '@/utils/routing'
 import Footer from './footer'
 import { Analytics } from '@vercel/analytics/next'
@@ -20,7 +20,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }>) {
   return <html lang='en'>
-    <body className={jost.className}>
+    <body style={{ fontFamily: `${jost.style.fontFamily}, ${chinese.style.fontFamily}` }}>
       <Analytics></Analytics>
       <Providers>
         <div className={`flex flex-col w-full ${await checkIsEmbedding() ? '' : 'bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#2D2E2F_1px,transparent_1px)] [background-size:20px_20px]'}`}>
--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,4 +1,5 @@
-import { Pacifico, Jost } from 'next/font/google'
+import { Pacifico, Jost, Ma_Shan_Zheng } from 'next/font/google'
 
 export const jost = Jost({ subsets: ['latin'], weight: 'variable' })
 export const pacifico = Pacifico({ subsets: ['latin'], weight: ['400'] })
+export const chinese = Ma_Shan_Zheng({ subsets: ['latin'], weight: ['400'] })
